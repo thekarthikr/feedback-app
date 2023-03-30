4e4391c8686c0840b9c84a467a0bc1b4feed759b
Karthik <karthikwebdev@outlook.com>
2023-03-30 19:12:52 +0530

    make header as link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,27 @@
-function Header({color, bgColor}){
-  
-    const headerStyles = {
-        color:color,
-        backgroundColor:bgColor
-    }
+import { Link } from "react-router-dom";
 
+function Header({ color, bgColor }) {
+  const headerStyles = {
+    color: color,
+    backgroundColor: bgColor,
+  };
 
-    return(
-        <header style={headerStyles}>
-            <div className="container">
-                <h1> Feedback App</h1>
-            </div>
-        </header>
-    )
+  return (
+    <header style={headerStyles}>
+      <div className='container'>
+        <h1>
+          <Link style={{ color }} to='/'>
+            Feedback App
+          </Link>
+        </h1>
+      </div>
+    </header>
+  );
 }
 
 Header.defaultProps = {
-    color:"#ff6a95",
-    bgColor:'rgba(0,0,0,.4)'
-}
+  color: "#ff6a95",
+  bgColor: "rgba(0,0,0,.4)",
+};
 
-export default Header
+export default Header;
